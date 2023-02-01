d777b17b6c67b348299738cca403c7723467601e
DESKTOP-MKH8SQO\quock <quockhanhtruong166@gmail.com>
2023-02-01 17:59:14 +0700

    90%

--- assets/js/form.js
+++ assets/js/form.js
@@ -1,39 +1,47 @@
 import { $, $$ } from "./app.js";
 
+const removeActive = (element, isDataPage = false, dataPage = null) =>
+  isDataPage
+    ? element
+        .querySelector(`[data-page="${dataPage}"]`)
+        .classList.remove("active")
+    : $(`.${element}.active`).classList.remove("active");
+
+const addActive = (element, isDataPage = false, dataPage = null) =>
+  isDataPage
+    ? element.querySelector(`[data-page="${dataPage}"]`).classList.add("active")
+    : $(`.${element}.active`).classList.add("active");
+
 export function dropDown() {
   const tableInputs = $$(".form-animal-table-content-input");
   tableInputs.forEach((tableInput) => {
     tableInput.querySelector("input").addEventListener("focusin", () => {
       $(".form-animal-table-content-input.active")
-        ? $(".form-animal-table-content-input.active").classList.remove(
-            "active"
-          )
-        : tableInput.classList.add("active");
+        ? removeActive("form-animal-table-content-input")
+        : "";
+      tableInput.classList.add("active");
     });
 
     tableInput.querySelector("input").addEventListener("focusout", () => {
       setTimeout(() => {
-        if (
-          $(".form-animal-table-content-dropdown input") ===
-          document.activeElement
-        ) {
-        } else {
-          $(".form-animal-table-content-dropdown input").addEventListener(
-            "focusout",
-            () => {
-              $(".form-animal-table-content-input.active")
-                ? $(".form-animal-table-content-input.active").classList.remove(
-                    "active"
-                  )
-                : "";
+        const dropDownInputs = $$(".form-animal-table-content-dropdown input");
+        let count = 0;
+        dropDownInputs.forEach((dropDownInput) => {
+          if (dropDownInput === document.activeElement) {
+            count--;
+          } else {
+            count++;
+            if (count == dropDownInputs.length) {
+              $(".form-animal-table-content-input.active") &&
+                removeActive("form-animal-table-content-input");
+            } else {
+              dropDownInput.addEventListener("focusout", () => {
+                $(".form-animal-table-content-input.active") &&
+                  removeActive("form-animal-table-content-input");
+              });
             }
-          );
-          $(".form-animal-table-content-input.active")
-            ? $(".form-animal-table-content-input.active").classList.remove(
-                "active"
-              )
-            : "";
-        }
+          }
+        });
       }, 10);
     });
   });
@@ -43,70 +51,44 @@ export function changePage() {
   let pageContainer;
   let pageTables;
   let animal = $(".form-animal-container.active").getAttribute("data-animal");
+
   pageContainer = $(
     `[class="form-animal-container active"][data-animal="${animal}"]`
   );
   pageTables = pageContainer.querySelectorAll(".form-animal-table-content");
+
+  const setPercents = (percentage) => {
+    pageContainer.querySelector(".form-animal-table-progress p").innerText =
+      percentage;
+    pageContainer.querySelector(
+      ".form-animal-table-progress-bar-current"
+    ).style.width = percentage;
+  };
+
   pageTables.forEach((pageTable) => {
     if (pageTable.querySelector(".next-button")) {
       pageTable.querySelector(".next-button").addEventListener("click", () => {
         if (pageTable.getAttribute("data-page") == "1") {
-          pageContainer
-            .querySelector('[data-page="1"]')
-            .classList.remove("active");
-          pageContainer
-            .querySelector('[data-page="2"]')
-            .classList.add("active");
-          pageContainer.querySelector(
-            ".form-animal-table-progress p"
-          ).innerText = "33%";
-          pageContainer.querySelector(
-            ".form-animal-table-progress-bar-current"
-          ).style.width = "33%";
+          removeActive(pageContainer, true, 1);
+          addActive(pageContainer, true, 2);
+          setPercents("33%");
         } else if (pageTable.getAttribute("data-page") == "2") {
-          pageContainer
-            .querySelector('[data-page="2"]')
-            .classList.remove("active");
-          pageContainer
-            .querySelector('[data-page="3"]')
-            .classList.add("active");
-          pageContainer.querySelector(
-            ".form-animal-table-progress p"
-          ).innerText = "67%";
-          pageContainer.querySelector(
-            ".form-animal-table-progress-bar-current"
-          ).style.width = "67%";
+          removeActive(pageContainer, true, 2);
+          addActive(pageContainer, true, 3);
+          setPercents("67%");
         }
       });
     }
     if (pageTable.querySelector(".back-button")) {
       pageTable.querySelector(".back-button").addEventListener("click", () => {
         if (pageTable.getAttribute("data-page") == "3") {
-          pageContainer
-            .querySelector('[data-page="3"]')
-            .classList.remove("active");
-          pageContainer
-            .querySelector('[data-page="2"]')
-            .classList.add("active");
-          pageContainer.querySelector(
-            ".form-animal-table-progress p"
-          ).innerText = "33%";
-          pageContainer.querySelector(
-            ".form-animal-table-progress-bar-current"
-          ).style.width = "33%";
+          removeActive(pageContainer, true, 3);
+          addActive(pageContainer, true, 2);
+          setPercents("33%");
         } else if (pageTable.getAttribute("data-page") == "2") {
-          pageContainer
-            .querySelector('[data-page="2"]')
-            .classList.remove("active");
-          pageContainer
-            .querySelector('[data-page="1"]')
-            .classList.add("active");
-          pageContainer.querySelector(
-            ".form-animal-table-progress p"
-          ).innerText = "0%";
-          pageContainer.querySelector(
-            ".form-animal-table-progress-bar-current"
-          ).style.width = "0%";
+          removeActive(pageContainer, true, 2);
+          addActive(pageContainer, true, 1);
+          setPercents("0%");
         }
       });
     }
@@ -120,42 +102,31 @@ export function toggleRadio() {
   );
   radios.forEach((radio) => {
     radio.addEventListener("click", () => {
-      if (
-        $(".form-animal-container.active").getAttribute("data-animal") == "dog"
-      ) {
+      const dogCatDisplay = (animal, all, part) => {
         formContainer = $(
-          '[class="form-animal-container active"][data-animal="dog"]'
+          `[class="form-animal-container active"][data-animal=${animal}]`
         );
-        if (radio.getAttribute("for") == "all") {
+        if (radio.getAttribute("for") == all) {
           formContainer.querySelector(".insurance-all-package").style.display =
             "block";
           formContainer.querySelector(".insurance-part-package").style.display =
             "none";
-        } else if (radio.getAttribute("for") == "part") {
+        } else if (radio.getAttribute("for") == part) {
           formContainer.querySelector(".insurance-all-package").style.display =
             "none";
           formContainer.querySelector(".insurance-part-package").style.display =
             "block";
         }
-      }
-      if (
-        $(".form-animal-container.active").getAttribute("data-animal") == "cat"
-      ) {
-        formContainer = $(
-          '[class="form-animal-container active"][data-animal="cat"]'
-        );
-        if (radio.getAttribute("for") == "all-cat") {
-          formContainer.querySelector(".insurance-all-package").style.display =
-            "block";
-          formContainer.querySelector(".insurance-part-package").style.display =
-            "none";
-        } else if (radio.getAttribute("for") == "part-cat") {
-          formContainer.querySelector(".insurance-all-package").style.display =
-            "none";
-          formContainer.querySelector(".insurance-part-package").style.display =
-            "block";
-        }
-      }
+      };
+
+      const dogCatAttribute = $(".form-animal-container.active").getAttribute(
+        "data-animal"
+      );
+
+      dogCatAttribute === "dog"
+        ? dogCatDisplay("dog", "all", "part")
+        : dogCatDisplay("cat", "all-cat", "part-cat");
+
       if (
         $(".form-animal-container.active").getAttribute("data-animal") ==
         "horse"
@@ -163,55 +134,52 @@ export function toggleRadio() {
         formContainer = $(
           '[class="form-animal-container active"][data-animal="horse"]'
         );
-        if (radio.getAttribute("for") == "basic-horse") {
-          formContainer.querySelector(
-            ".insurance-basic-package"
-          ).style.display = "block";
-          formContainer.querySelector(
-            ".insurance-premium-package"
-          ).style.display = "none";
-          formContainer.querySelector(
-            ".insurance-premium-plus-package"
-          ).style.display = "none";
-          formContainer.querySelector(".insurance-inkl-package").style.display =
-            "none";
-        } else if (radio.getAttribute("for") == "premium-horse") {
-          formContainer.querySelector(
-            ".insurance-basic-package"
-          ).style.display = "none";
-          formContainer.querySelector(
-            ".insurance-premium-package"
-          ).style.display = "block";
-          formContainer.querySelector(
-            ".insurance-premium-plus-package"
-          ).style.display = "none";
-          formContainer.querySelector(".insurance-inkl-package").style.display =
-            "none";
-        } else if (radio.getAttribute("for") == "premium-plus-horse") {
-          formContainer.querySelector(
-            ".insurance-basic-package"
-          ).style.display = "none";
-          formContainer.querySelector(
-            ".insurance-premium-package"
-          ).style.display = "none";
-          formContainer.querySelector(
-            ".insurance-premium-plus-package"
-          ).style.display = "block";
-          formContainer.querySelector(".insurance-inkl-package").style.display =
-            "none";
-        } else if (radio.getAttribute("for") == "inkl-horse") {
-          formContainer.querySelector(
-            ".insurance-basic-package"
-          ).style.display = "none";
-          formContainer.querySelector(
-            ".insurance-premium-package"
-          ).style.display = "none";
-          formContainer.querySelector(
-            ".insurance-premium-plus-package"
-          ).style.display = "none";
-          formContainer.querySelector(".insurance-inkl-package").style.display =
-            "block";
-        }
+
+        const setDisplay = () => {
+          const packageArr = [
+            "insurance-basic-package",
+            "insurance-premium-package",
+            "insurance-premium-plus-package",
+            "insurance-inkl-package",
+          ];
+          const labelArr = [
+            "basic-horse",
+            "premium-horse",
+            "premium-plus-horse",
+            "inkl-horse",
+          ];
+
+          if (labelArr.includes(`${radio.getAttribute("for")}`)) {
+            packageArr.forEach((el) => {
+              formContainer.querySelector(`.${el}`).style.display = "none";
+            });
+          }
+
+          switch (radio.getAttribute("for")) {
+            case "basic-horse":
+              formContainer.querySelector(
+                ".insurance-basic-package"
+              ).style.display = "block";
+              break;
+            case "premium-horse":
+              formContainer.querySelector(
+                ".insurance-premium-package"
+              ).style.display = "block";
+              break;
+            case "premium-plus-horse":
+              formContainer.querySelector(
+                ".insurance-premium-plus-package"
+              ).style.display = "block";
+              break;
+            case "inkl-horse":
+              formContainer.querySelector(
+                ".insurance-inkl-package"
+              ).style.display = "block";
+              break;
+          }
+        };
+
+        setDisplay();
       }
     });
   });
@@ -221,33 +189,14 @@ export function changeForm() {
   const forms = $$("#form .form-animal-selection .form-animal-selection-item");
   forms.forEach((form) => {
     form.addEventListener("click", () => {
-      if (form.getAttribute("data-animal") == "dog") {
-        $(".form-animal-selection-item.active").classList.remove("active");
-        form.classList.add("active");
-        $(".form-animal-container.active").classList.remove("active");
-        $('[data-animal="dog"][class="form-animal-container"]').classList.add(
-          "active"
-        );
-        changePage();
-      }
-      if (form.getAttribute("data-animal") == "cat") {
-        $(".form-animal-selection-item.active").classList.remove("active");
-        form.classList.add("active");
-        $(".form-animal-container.active").classList.remove("active");
-        $('[data-animal="cat"][class="form-animal-container"]').classList.add(
-          "active"
-        );
-        changePage();
-      }
-      if (form.getAttribute("data-animal") == "horse") {
-        $(".form-animal-selection-item.active").classList.remove("active");
-        form.classList.add("active");
-        $(".form-animal-container.active").classList.remove("active");
-        $('[data-animal="horse"][class="form-animal-container"]').classList.add(
-          "active"
-        );
-        changePage();
-      }
+      const animal = form.getAttribute("data-animal");
+      removeActive("form-animal-selection-item");
+      form.classList.add("active");
+      removeActive("form-animal-container");
+      $(`[data-animal=${animal}][class="form-animal-container"]`).classList.add(
+        "active"
+      );
+      changePage();
     });
   });
 }
